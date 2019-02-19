2812cadde6a2ace305f6e8705e2f3123cb33eade
Alex Hoyau <a.hoyau@silexlabs.org>
2019-02-18 20:40:19 -0600

    Batch update

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@
 var OFFSET = -100;
 $(function() {
     var buttons = $('.hero-section-scroll-down');
-    console.log('xxx', buttons);
     buttons.click(function() {
         var $section = $($(this).parents('.section-element')[0]);
         $('html, body').animate({ scrollTop: $section.prop("scrollHeight") + $section.offset().top + OFFSET}, 1000);
